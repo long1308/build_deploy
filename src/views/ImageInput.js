66c895c5dfa2aca04d6d3c41dea737804e63fecf
long1308 <lemle2580@gmail.com>
2023-08-02 21:07:10 +0700

    update input image

--- src/views/ImageInput.js
+++ src/views/ImageInput.js
@@ -1,20 +1,21 @@
-import React, { Component } from 'react';
-import { withRouter } from 'react-router-dom';
-import { loadModels, getFullFaceDescription, createMatcher } from '../api/face';
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import { loadModels, getFullFaceDescription, createMatcher } from "../api/face";
 
 // Import image to test API
-const testImg = require('../img/test.jpg');
+// const testImg = require("../img/test.jpg");
 
 // Import face profile
-const JSON_PROFILE = require('../descriptors/bnk48.json');
+const JSON_PROFILE = require("../descriptors/bnk48.json");
 
 // Initial State
 const INIT_STATE = {
-  imageURL: testImg,
+  imageURL: null,
   fullDesc: null,
   detections: null,
   descriptors: null,
-  match: null
+  match: null,
+  hasDetectedFace: false,
 };
 
 class ImageInput extends Component {
@@ -23,6 +24,10 @@ class ImageInput extends Component {
     this.state = { ...INIT_STATE, faceMatcher: null };
   }
 
+  checkIfFaceDetected = async (image) => {
+    const fullDesc = await getFullFaceDescription(image);
+    return !!fullDesc && fullDesc.length > 0; // Trả về true nếu có khuôn mặt được phát hiện, ngược lại trả về false
+  };
   componentWillMount = async () => {
     await loadModels();
     this.setState({ faceMatcher: await createMatcher(JSON_PROFILE) });
@@ -30,29 +35,30 @@ class ImageInput extends Component {
   };
 
   handleImage = async (image = this.state.imageURL) => {
-    await getFullFaceDescription(image).then(fullDesc => {
+    await getFullFaceDescription(image).then((fullDesc) => {
       if (!!fullDesc) {
         this.setState({
           fullDesc,
-          detections: fullDesc.map(fd => fd.detection),
-          descriptors: fullDesc.map(fd => fd.descriptor)
+          detections: fullDesc.map((fd) => fd.detection),
+          descriptors: fullDesc.map((fd) => fd.descriptor),
+          hasDetectedFace: fullDesc.length > 0,
         });
       }
     });
 
     if (!!this.state.descriptors && !!this.state.faceMatcher) {
-      let match = await this.state.descriptors.map(descriptor =>
+      let match = await this.state.descriptors.map((descriptor) =>
         this.state.faceMatcher.findBestMatch(descriptor)
       );
       this.setState({ match });
     }
   };
 
-  handleFileChange = async event => {
+  handleFileChange = async (event) => {
     this.resetState();
     await this.setState({
       imageURL: URL.createObjectURL(event.target.files[0]),
-      loading: true
+      loading: true,
     });
     this.handleImage();
   };
@@ -62,9 +68,15 @@ class ImageInput extends Component {
   };
 
   render() {
-    const { imageURL, detections, match } = this.state;
+    const { imageURL, detections, match, hasDetectedFace } = this.state;
 
     let drawBox = null;
+    let errorMessage = null;
+    if (!hasDetectedFace) {
+      errorMessage = (
+        <p>No faces were detected in the image to be reselected</p>
+      );
+    }
     if (!!detections) {
       drawBox = detections.map((detection, i) => {
         let _H = detection.box.height;
@@ -75,24 +87,24 @@ class ImageInput extends Component {
           <div key={i}>
             <div
               style={{
-                position: 'absolute',
-                border: 'solid',
-                borderColor: 'blue',
+                position: "absolute",
+                border: "solid",
+                borderColor: "blue",
                 height: _H,
                 width: _W,
-                transform: `translate(${_X}px,${_Y}px)`
+                transform: `translate(${_X}px,${_Y}px)`,
               }}
             >
               {!!match ? (
                 <p
                   style={{
-                    backgroundColor: 'blue',
-                    border: 'solid',
-                    borderColor: 'blue',
+                    backgroundColor: "blue",
+                    border: "solid",
+                    borderColor: "blue",
                     width: _W,
                     marginTop: 0,
-                    color: '#fff',
-                    transform: `translate(-3px,${_H}px)`
+                    color: "#fff",
+                    transform: `translate(-3px,${_H}px)`,
                   }}
                 >
                   {match[i]._label}
@@ -112,12 +124,16 @@ class ImageInput extends Component {
           onChange={this.handleFileChange}
           accept=".jpg, .jpeg, .png"
         />
-        <div style={{ position: 'relative' }}>
-          <div style={{ position: 'absolute' }}>
-            <img src={imageURL} alt="imageURL" />
+        {errorMessage ? (
+          errorMessage
+        ) : (
+          <div style={{ position: "relative" }}>
+            <div style={{ position: "absolute" }}>
+              <img src={imageURL} alt="imageURL" />
+            </div>
+            {!!drawBox ? drawBox : null}
           </div>
-          {!!drawBox ? drawBox : null}
-        </div>
+        )}
       </div>
     );
   }
