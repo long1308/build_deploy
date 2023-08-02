0eea35e08388d282852634c22cb7c6066f78f204
long1308 <lemle2580@gmail.com>
2023-08-02 21:33:50 +0700

    Updates

---
+++ precache-manifest.6c36ba55d73dc6572051a3bf23f512cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a003fad180654dbed52",
+    "url": "/build/static/css/main.9aba82bd.chunk.css"
+  },
+  {
+    "revision": "9a003fad180654dbed52",
+    "url": "/build/static/js/main.9a003fad.chunk.js"
+  },
+  {
+    "revision": "4903a77df3ae1659d180",
+    "url": "/build/static/js/1.4903a77d.chunk.js"
+  },
+  {
+    "revision": "3923d73d01a4271659cc",
+    "url": "/build/static/js/runtime~main.3923d73d.js"
+  },
+  {
+    "revision": "a96f3a453a1871caddc29171f573fdaf",
+    "url": "/build/index.html"
+  }
+];
